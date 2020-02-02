feac21d7e9a355a9307c94b8098c0091552bce5e
graziela silva <grazims26@gmail.com>
2020-02-01 16:28:06 -0800

    add sectionlist

--- js/screens/Schedule/Schedule.js
+++ js/screens/Schedule/Schedule.js
@@ -4,10 +4,30 @@ import Logo from '../../components/logo';
 import globalStyles from '../globalStyles';
 import styles from './styles';
 import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+const timeFormatter = time =>
+  new Date(time).toLocaleString('en-US', {hour: 'numeric', hour12: true});
+
+const sessionGrouper = (headers, session) => {
+  const sectionIndex = headers.findIndex(
+    ({title}) => title === session.startTime,
+  );
+
+  if (sectionIndex === -1)
+    return [
+      ...headers,
+      {
+        title: session.startTime,
+        data: [session],
+      },
+    ];
+
+  headers[sectionIndex].data.push(session);
+
+  return headers;
+};
 
 const ScheduleItem = ({item}) => (
   <View>
-    <Text style={styles.h2Date}>{item.startTime}</Text>
     <Text style={styles.titleSche}>{item.title}</Text>
     <Text style={styles.h2Loc}>{item.location}</Text>
   </View>
@@ -19,10 +39,14 @@ const Schedule = ({startTime, title, location, data}) => {
       <Logo />
       {/* //REMOVER O LOGO */}
 
-      <FlatList
-        data={data}
+      <SectionList
+        sections={data.reduce(sessionGrouper, [])}
+        // data={data}
         renderItem={({item}) => <ScheduleItem item={item} />}
         keyExtractor={item => item.id}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={styles.h2Date}>{timeFormatter(title)}</Text>
+        )}
       />
     </View>
   );
@@ -30,3 +54,6 @@ const Schedule = ({startTime, title, location, data}) => {
 
 export default Schedule;
 //fazer uma session list
+//fazer uma lista com as sessoes diferentes como se fosse uma array
+
+//lembrar de checar o fim da sessao. se vai até o final
